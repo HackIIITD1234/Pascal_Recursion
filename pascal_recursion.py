1272ef06c672ba73004c377f277522bf875a5261
Chaitanya Satsangi <chaitanya24158@iiitd.ac.in>
2024-11-17 15:11:39 +0530

    Add files via upload

---
+++ pascal_recursion.py
@@ -0,0 +1,12 @@
+def pascal(n, l = [1]):
+    print(l)
+    if len(l) == n:
+        return l
+    l_ = [1] + [l[i] + l[i+1] for i in range(len(l)-1)] + [1]
+    # l_ = []
+    # l_.append(l[0])
+    # for i in range(len(l)):
+    #     l_.append(sum(l[i:i+2]))
+    return pascal(n, l_)
+
+pascal(0)
